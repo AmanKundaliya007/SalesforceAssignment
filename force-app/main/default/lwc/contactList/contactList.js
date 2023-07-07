648584192791dcdb7d3f8653d3178a1b847b8912
Aman Kundalia <aman.kundalia@genesisenergy.co.nz>
2023-07-07 20:14:25 +0530

    Description added

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -2,32 +2,28 @@ import { LightningElement, wire } from 'lwc';
 
 import contactList from '@salesforce/apex/ContactController.contactList';
 const cols = [
-    {label:'First Name', fieldName:'FirstName',type:'text'},
-    {label:'Last Name', fieldName:'LastName',type:'text'},
-    {label:'Email', fieldName:'Email',type:'Email'},
+{label:'First Name', fieldName:'FirstName',type:'text'},
+{label:'Last Name', fieldName:'LastName',type:'text'},
+{label:'Email', fieldName:'Email',type:'Email'},
 
 ];
 
 export default class ContactList extends LightningElement {
 
-    contactRecords;
-    errors;
-    columns = cols;
-    
-
-    @wire(contactList)
-        wiredContacts({data,error}){
-            if(data){
-                console.log('data',data);
-                this.contactRecords = data;
-                console.log('contactRecords',this.contactRecords);
-                this.errors = undefined;
-            }
-            if(error){
-                this.errors = error;
-                this.data = undefined;
-            }
-        }
+contactRecords;
+errors;
+columns = cols;
 
 
+@wire(contactList)
+wiredContacts({data,error}){
+    if(data){
+        this.contactRecords = data;
+        this.errors = undefined;
+    }
+    if(error){
+        this.errors = error;
+        this.data = undefined;
+    }
+}
 }
